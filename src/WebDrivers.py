b1021e92d94ffc9b80fa02e1b6176a963f6bc91e
van-1997 <van.pogosyan.1997@gmail.com>
2024-09-08 12:31:32 +0400

    add try except

--- src/WebDrivers.py
+++ src/WebDrivers.py
@@ -8,45 +8,59 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
-
 def read_json(file_path):
+    try:
         with open(file_path, "r") as file:
             json_data = json.load(file)
         return json_data
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
 
 
 class WebDriverFactory:
     def __init__(self, json_data):
-        self.json_data = json_data
+        self.driver_type = json_data.get("driver_type", "").lower()
+        self.screen_width = json_data.get("width")
+        self.screen_height = json_data.get("height")
+
 
     def driver(self, url):
-        driver_type = self.json_data.get("driver_type", "").lower()
-        screen_width = self.json_data.get("weight")
-        screen_height = self.json_data.get("height")
 
-        if driver_type == "chrome":
+        if self.driver_type == "chrome":
             chrome_options = ChromeOptions()
             service = ChromeService(ChromeDriverManager().install())
             driver = webdriver.Chrome(service=service, options=chrome_options)
-            driver.set_window_size(screen_width, screen_height)
+
+            if self.screen_width and self.screen_height:
+                driver.set_window_size(self.screen_width, self.screen_height)
+            else:
+                driver.maximize_window()
+
             driver.get(url)
             return driver
 
-        elif driver_type == "firefox":
+        elif self.driver_type == "firefox":
             firefox_options = FirefoxOptions()
             service = FirefoxService(GeckoDriverManager().install())
             driver = webdriver.Firefox(service=service, options=firefox_options)
-            driver.set_window_size(screen_width, screen_height)
+
+            if self.screen_width and self.screen_height:
+                driver.set_window_size(self.screen_width, self.screen_height)
+            else:
+                driver.maximize_window()
+
             driver.get(url)
             return driver
 
-file_path = "/home/vanya/PycharmProjects/selenium_framework/global_vars.json"
+
+file_path = "/home/vanya/PycharmProjects/selenium_framework/global_vars1.json"
 json_data = read_json(file_path)
-webdriver_factory = WebDriverFactory(json_data)
 
-url = "https://www.facebook.com"
-
-driver = webdriver_factory.driver(url)
-time.sleep(5)
-driver.quit()
-
+if json_data:
+    webdriver_factory = WebDriverFactory(json_data)
+    url = "https://www.facebook.com"
+    driver = webdriver_factory.driver(url)
+    time.sleep(5)
+    driver.quit()
+else:
+    print("Fayl ne nayden")
